b697dda13541c488d04f6d5d03d512a68f395418
Adrian Robitu <arobitu@ifdsgroup.com>
2016-09-21 13:41:30 -0400

    Added the core drivers.

--- src/main/java/com/ifdsgroup/artemismq/sample/standard/ArtemisHelper.java
+++ src/main/java/com/ifdsgroup/artemismq/sample/standard/ArtemisHelper.java
@@ -7,6 +7,8 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.api.core.client.ActiveMQClient;
+import org.apache.activemq.artemis.api.core.client.ServerLocator;
 import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
 import org.apache.activemq.artemis.api.jms.JMSFactoryType;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
@@ -16,15 +18,27 @@ public class ArtemisHelper {
 
 	private static final String HOST_NAME = "ifds4150";
 	private static final String QUEUE_NAME = "OrderQueue";
+	
+	private static TransportConfiguration transport = null;
+	
+	private static synchronized TransportConfiguration getTransport() {
+		if (ArtemisHelper.transport == null) {
+			Map<String, Object> connectionParams = new HashMap<>();
+			connectionParams.put(TransportConstants.HOST_PROP_NAME, HOST_NAME);
+			ArtemisHelper.transport = new TransportConfiguration(NettyConnectorFactory.class.getName(), connectionParams);
+		}
+		return ArtemisHelper.transport;
+	}
 
 	public static ConnectionFactory getConnectionFactory() {
-		Map<String, Object> connectionParams = new HashMap<>();
-		connectionParams.put(TransportConstants.HOST_PROP_NAME, HOST_NAME);
-		TransportConfiguration transport = new TransportConfiguration(NettyConnectorFactory.class.getName(), connectionParams);
-		return ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, transport);
+		return ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, getTransport());
 	}
 	
 	public static Queue getQueue() {
 		return ActiveMQJMSClient.createQueue(QUEUE_NAME);
+	}
+
+	public static ServerLocator getServerLocator() {
+		return ActiveMQClient.createServerLocatorWithoutHA(getTransport());
 	}
 }
